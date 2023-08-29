3d4241e3826e8b1baecaa5c216e2540997fa65fd
Jstreetman <137229312+Jstreetman@users.noreply.github.com>
2023-08-29 14:52:38 -0400

    Update database.js

--- database.js
+++ database.js
@@ -1,14 +1,9 @@
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-  host: "portfoliodb-do-user-14544783-0.b.db.ondigitalocean.com",
-  user: "doadmin",
-  password: "AVNS_3OkZakYxe5kWh0AN7fi",
-  port: 25060,
-  database: "defaultdb",
-  // host: "localhost",
-  // user: "root",
-  // password: "Ambassadors22!",
-  // port: 3306,
-  // database: "customersdb",
+   host: "localhost",
+   user: "root",
+   password: "Ambassadors22!",
+   port: 3306,
+   database: "defaultdb",
 });
